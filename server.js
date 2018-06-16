6d92ff9f59314651a659de88fb2c7e3c698bf21c
Joe McCreesh <jmccreesh@libertyintegrated.com>
2018-06-16 12:34:04 -0400

    Updates mongo connection.

--- server.js
+++ server.js
@@ -28,7 +28,13 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/scraper");
+// mongoose.connect("mongodb://localhost/scraper");
+
+
+const MONGO = process.env.MONGODB_URI || 'mongodb://localhost/nyt';
+
+mongoose.connect(MONGO);
+
 var db = mongoose.connection;
 
 // Show any mongoose errors
